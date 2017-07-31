99f9df817a020c4c047a6f184c3af6a48b23087d
Alex <golearnweb@gmail.com>
2017-07-31 14:40:51 +0300

    commit

---
+++ messenger.js
@@ -0,0 +1,36 @@
+function attachEvents() {
+    $("#submit").click(attachSend);
+    $("#refresh").click(attachRefresh);
+
+    function attachSend() {
+        let messageJson = {
+            "author": $("#author").val(),
+            "content": $("#content").val(),
+            "timestamp": Date.now()
+        };
+
+        let sendMessageRequest = {
+            url: "https://messenger-e76f7.firebaseio.com/messenger.json",
+            method: "POST",
+            data: JSON.stringify(messageJson),
+            success: attachRefresh
+        };
+
+        $.ajax(sendMessageRequest);
+    }
+
+    function attachRefresh() {
+        $.get("https://messenger-e76f7.firebaseio.com/messenger.json")
+            .then(displayMessages);
+    }
+
+    function displayMessages(messages) {
+        let output = $("#messages");
+        output.empty();
+        let messagesStr = "";
+        for (let key in messages) {
+            messagesStr += `${messages[key]["author"]}: ${messages[key]["content"]}\n`
+        }
+        output.text(messagesStr);
+    }
+}
